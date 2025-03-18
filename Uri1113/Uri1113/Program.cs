4fca4bc58e73e75f1061f04bd74992842794a07c
keven souza grillo <54678344+Kievgrillo@users.noreply.github.com>
2025-03-18 18:09:32 -0300

    Add files via upload

---
+++ Uri1113/Uri1113/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri1113
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X, Y;
+
+            string[] vet = Console.ReadLine().Split(' ');
+            X = int.Parse(vet[0]);
+            Y = int.Parse(vet[1]);
+
+            while (X != Y)
+            {
+                if (X > Y)
+                {
+                    Console.WriteLine("Decrescente");
+                }
+                else
+                {
+                    Console.WriteLine("Crescente");
+                }
+                vet = Console.ReadLine().Split(' ');
+                X = int.Parse(vet[0]);
+                Y = int.Parse(vet[1]);
+            }
+        }
+    }
+}
