e6ee70829525c248e4ad7c396de713a0f93dd15e
keven souza grillo <54678344+Kievgrillo@users.noreply.github.com>
2025-03-17 21:31:06 -0300

    Add files via upload

---
+++ Uri1020/Uri1020/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+using System.Globalization;
+
+namespace Uri1020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X, meses, anos, dias, resto;
+
+            X = int.Parse(Console.ReadLine());
+
+            anos = X / 365;
+            resto = X % 365;
+            meses = resto / 30;
+            dias = resto % 30;
+
+            Console.WriteLine(anos + " ano(s)");
+            Console.WriteLine(meses + " mes(es)");
+            Console.WriteLine(dias + " dia(s)");
+        }
+    }
+}
---
+++ Uri1036/URI1036/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Exemplo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Delta, A,B,C, r1, r2;
+
+            String[] vet = Console.ReadLine().Split(' ');
+            A = double.Parse(vet[0], CultureInfo.InvariantCulture);
+            B = double.Parse(vet[1], CultureInfo.InvariantCulture);
+            C = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            Delta = Math.Pow(B, 2.0) - 4 * A * C;
+
+            if (A == 0 || Delta < 0.0)
+            {
+                Console.WriteLine("Impossivel calcular");
+            } 
+            else
+            {
+                r1 = (-B + Math.Sqrt(Delta)) / (2.0 * A);
+                r2 = (-B - Math.Sqrt(Delta)) / (2.0 * A);
+                Console.WriteLine("R1 = " + r1.ToString("F5", CultureInfo.InvariantCulture));
+                Console.WriteLine("R2 = " + r2.ToString("F5", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
