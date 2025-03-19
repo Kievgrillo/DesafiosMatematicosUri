f719d86bc4302f51bf63c3836d701f8dd6edbd7e
keven souza grillo <54678344+Kievgrillo@users.noreply.github.com>
2025-03-19 13:44:40 -0300

    Add files via upload

---
+++ Uri1071/Uri1071/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Uri1071
+{
+    class Prgram
+    {
+        static void Main(string[] args)
+        {
+            int X, Y, Max, Min, Soma;
+
+            X = int.Parse(Console.ReadLine());
+            Y = int.Parse(Console.ReadLine());
+
+            if (X < Y)
+            {
+                Min = X;
+                Max = Y;
+            }
+            else
+            {
+                Min = Y;
+                Max = X;
+            }
+
+            Soma = 0;
+            for (int i = Min + 1; i < Max; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Soma = Soma + i;
+                }
+            }
+            Console.WriteLine(Soma);
+        }
+    }
+}
---
+++ Uri1078/Uri1078/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Uri1078
+{
+    class Prgram
+    {
+        static void Main(string[] args)
+        {
+            int N; 
+
+            N = int.Parse(Console.ReadLine());
+
+            for (int i=1; i <= 10; i++)
+            {
+                int Resultado = i * N;
+                Console.WriteLine( i + " x " + N + " = " + Resultado );
+            }
+        }
+    }
+}
